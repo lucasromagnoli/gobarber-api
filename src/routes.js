f206553e77c9ca532d063bffcaa69242979879a9
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2019-07-18 21:39:52 -0300

    Eslint and Editorconfig

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "GoBarbers" });
-});
+routes.get('/', (req, res) => res.json({ message: 'GoBarbers' }));
 
 export default routes;
