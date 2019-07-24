846c748973fc5a0641460df392854bb8751d7942
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2019-07-23 22:13:48 -0300

    Config mongoose and mogodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,10 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 import databaseConfig from '../config/database';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
+import mongoDatabaseConfig from '../config/mongoDatabase';
 
 const models = [User, File, Appointment];
 
@@ -17,6 +19,13 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      mongoDatabaseConfig.url,
+      mongoDatabaseConfig.config
+    );
   }
 }
 
