7133526bf00169fb0571325971f0944487d31a41
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2019-07-23 23:03:06 -0300

    Read a notification

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -21,6 +21,16 @@ class NotificationController {
 
     return res.json(notifications);
   }
+
+  async update(req, res) {
+    const nofication = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+
+    return res.json(nofication);
+  }
 }
 
 export default new NotificationController();
--- src/routes.js
+++ src/routes.js
@@ -27,5 +27,5 @@ routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
-
+routes.put('/notifications/:id', NotificationController.update);
 export default routes;
