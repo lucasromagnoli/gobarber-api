2ead9338f7e11b3a8b5b9bffd46558f974d138c1
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2019-07-24 22:32:46 -0300

    Sentry errors in productions and Youch to better errors info

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
 import express from 'express';
 import { resolve } from 'path';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+import serntryConfig from './config/sentry';
+import 'express-async-errors';
 import routes from './routes';
-
 import './database';
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(serntryConfig);
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -22,6 +28,16 @@ class App {
 
   routes() {
     this.server.use(routes);
+
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://2302c61d40b14ce7887ae992a4955fd6@sentry.io/1513017',
+};
